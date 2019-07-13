df907c09b2167deb2c5d0d7a4f87dfcf33901911
Damien Buttler <damien@doublehops.com>
2019-07-13 11:30:29 +1000

    Parsing and looping though JSON data.

--- watch.py
+++ watch.py
@@ -6,4 +6,9 @@ import config
 
 client = BTCMarkets (config.api_key, config.private_key)
 
-print(client.get_account_balance())
+data = client.get_account_balance()
+#print(data)
+
+for cur in data:
+    if cur['balance'] != 0:
+        print(cur['currency'] +': '+ str(cur['balance']))
